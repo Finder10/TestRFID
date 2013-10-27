4f0a3f1766120e54e13f1119787fa3127bcf443a
Finder10 <tong.kvin@gmail.com>
2013-10-27 19:51:53 +0100

    Test de la présence du NFC sur le mobile

--- src/com/example/testrfid/AccueilTestRFID.java
+++ src/com/example/testrfid/AccueilTestRFID.java
@@ -1,15 +1,30 @@
 package com.example.testrfid;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class AccueilTestRFID extends Activity {
-
+public class AccueilTestRFID extends Activity  {
+	
+	NfcAdapter mNfcAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accueil_test_rfid);
+		
+		// Test de la présence du nfc sur le mobile
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC non disponible =(", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        else {
+        	Toast.makeText(this, "NFC disponible =)", Toast.LENGTH_LONG).show();
+        }
 	}
 
 	@Override
